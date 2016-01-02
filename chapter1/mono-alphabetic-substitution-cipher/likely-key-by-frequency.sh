8064a000a0adbfed3b17ab3b475128b60941539c
Doug Richardson <dougie.richardson@gmail.com>
2016-01-02 00:26:13 -0800

    wip

--- chapter1/mono-alphabetic-substitution-cipher/likely-key-by-frequency.sh
+++ chapter1/mono-alphabetic-substitution-cipher/likely-key-by-frequency.sh
@@ -5,6 +5,10 @@ set -e
 go build frequency_distribution.go
 go build transpose.go
 
+echo SORTED FREQUENCIES
+paste -d " " <(cat ciphertext | ./frequency_distribution|tail -n +3|sort -k 2 -n -r) <(cat english_text_stats.txt|sort -n -k 2 -r)|./transpose|./transpose
+
+echo KEY
 paste \
     <(cat english_text_stats.txt |sort -k 2 -n -r|cut -f 1 -d ' ') \
     <(cat ciphertext |./frequency_distribution|tail -n +3|sort -k 2 -n -r|cut -f 1 -d ' ') \
