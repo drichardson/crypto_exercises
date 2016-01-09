f581811ba12620ef41a6b4f9c32ad13526b1acb4
Doug Richardson <dougie.richardson@gmail.com>
2016-01-08 21:31:51 -0800

    remove complicated cruft

--- intro-modern-crypto/1.1/likely-character-substitutions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-freqdist=$(mktemp)
-cat ciphertext |go run frequency_distribution.go > "$freqdist"
-paste <(cat "$freqdist"|cut -f 1) <(cat "$freqdist"|cut -f 3|go run english_statistical_guess.go)
--- intro-modern-crypto/1.1/likely-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-./likely-character-substitutions.sh |go run transpose.go |tr -d ' '
