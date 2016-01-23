7455f9ca3176a11c0cbee11b695c52f3e2ddae43
Doug Richardson <dougie.richardson@gmail.com>
2016-01-22 23:35:04 -0800

    more optimizations

--- crypto-engineering/5.2/5.3/birthday_attack_optimized.go
+++ crypto-engineering/5.2/5.3/birthday_attack_optimized.go
@@ -12,7 +12,7 @@ import (
 	"runtime/pprof"
 )
 
-var nBits = flag.Int("n", 8, "Number of bits. Must be multiple of 8 between 8 and 48 inclusive.")
+var nBits = flag.Uint("n", 8, "Number of bits. Must be multiple of 8 between 8 and 48 inclusive.")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 var memprofile = flag.String("memprofile", "", "write memory profile to this file")
 
@@ -43,12 +43,12 @@ func main() {
 	}
 
 	// Hash input
-	h, err := NewSha512n(*nBits)
+	h, err := NewSha512n(int(*nBits))
 	if err != nil {
 		fmt.Println("Error creating sha512n.", err)
 		os.Exit(1)
 	}
-	item1, item2, digest := birthdayAttack(h)
+	item1, item2, digest := birthdayAttack(h, *nBits)
 	if item1 != nil {
 		fmt.Println("Found collision:")
 		fmt.Println("item1: ", hex.EncodeToString(item1))
@@ -60,30 +60,15 @@ func main() {
 	}
 }
 
-func uint64ToBytes(u uint64) []byte {
-	return []byte{
-		byte(u >> 56 & 255),
-		byte(u >> 48 & 255),
-		byte(u >> 40 & 255),
-		byte(u >> 32 & 255),
-		byte(u >> 24 & 255),
-		byte(u >> 16 & 255),
-		byte(u >> 8 & 255),
-		byte(u & 255),
-	}
-}
-
-func byte8ToUInt64(b [8]byte) uint64 {
-	b0 := uint64(b[0]) << 56
-	b1 := uint64(b[1]) << 48
-	b2 := uint64(b[2]) << 40
-	b3 := uint64(b[3]) << 32
-	b4 := uint64(b[4]) << 24
-	b5 := uint64(b[5]) << 16
-	b6 := uint64(b[6]) << 8
-	b7 := uint64(b[7]) << 0
-
-	return b0 | b1 | b2 | b3 | b4 | b5 | b6 | b7
+func uint64ToBytes(u uint64, out []byte) {
+	out[0] = byte(u >> 56 & 255)
+	out[1] = byte(u >> 48 & 255)
+	out[2] = byte(u >> 40 & 255)
+	out[3] = byte(u >> 32 & 255)
+	out[4] = byte(u >> 24 & 255)
+	out[5] = byte(u >> 16 & 255)
+	out[6] = byte(u >> 8 & 255)
+	out[7] = byte(u & 255)
 }
 
 func digestToIndex(digest []byte) uint64 {
@@ -95,7 +80,7 @@ func digestToIndex(digest []byte) uint64 {
 }
 
 // Perform birthday attack on 8, 16, 24, 32, 40, or 48 bit SHA-512-n digest.
-func birthdayAttack(h hash.Hash) (item1, item2, digestOut []byte) {
+func birthdayAttack(h hash.Hash, nBits uint) (item1, item2, digestOut []byte) {
 	// i is 64 bits. Since 2^64 > 2^48 (the largest SHA-512-n digest)
 	// at least one collision will be found due to the pigeon hole principle.
 	// There's a ~50% chance of finding such a collision in the first 2^(n/2)
@@ -103,91 +88,56 @@ func birthdayAttack(h hash.Hash) (item1, item2, digestOut []byte) {
 
 	type try struct {
 		data   [8]byte
-		digest [8]byte
+		digest [6]byte
+		set    bool
 	}
 	//tries := make(map[string][]byte)
 	// 2^24 = sqrt(2^48), so having that many entries lets us get to
 	// ~50% chance of successful attack. Once we've reached that point,
 	// the table will start to degrade like a chained hash table.
-	tableSize := uint(24) // in bits
-	//tableIndexMask := ^uint64(0) >> tableSize
-	tableIndexMask := (uint64(2) << tableSize) - 1
-	tries := make([]try, 2<<tableSize)
-	//digestBytes := h.Size()
+	// sqrt(2^n) == 2^(n/2) == 1 << (n/2) == 1 << (n >> 1).
+	tableSize := uint64(1 << (nBits >> 1))
+	tableSize <<= 2 // make the table times bigger to reduce false index collisions from ~60% to ~10%
+	tableIndexMask := tableSize - 1
+	tries := make([]try, tableSize)
+	fmt.Printf("Table len is %d. tableIndexMask is %x, nBits: %d, tableSize: %d\n", len(tries), tableIndexMask, nBits, tableSize)
 
 	// Loop below doesn't try i == 0. That's okay since we really only need to hit 2^48 different
 	// values, and the index is a uint64.
 
-	for i := uint64(1); i != 0; i++ {
-		data := uint64ToBytes(i)
-		h.Reset()
-		h.Write(data)
-		digest := h.Sum(nil)
-		//digest := []byte{byte(i % 255)}
+	//var digestBuf [8]byte
+	var dataBuf [8]byte
+	var indexCollisionCounter int
+	digestLen := nBits / 8
+	var digest512 [sha512.Size]byte
+
+	stopValue := uint64(1) << nBits
+
+	for i := uint64(1); i < stopValue; i++ {
+		data := dataBuf[:]
+		uint64ToBytes(i, data)
+		digest512 = sha512.Sum512(data)
+		digest := digest512[:digestLen]
 
 		tableIndex := digestToIndex(digest) & tableIndexMask
 		//fmt.Println("index", i, "tableIndex", tableIndex)
-		if bytes.Equal(tries[tableIndex].digest[:len(digest)], digest) {
+		tableDigest := tries[tableIndex].digest[:digestLen]
+		if bytes.Equal(tableDigest, digest) {
 			item1 = data
 			item2 = tries[tableIndex].data[:]
 			digestOut = digest
+			// If indexCollisionCounter is significant relative to the size of the search space, you need a bigger table.
+			fmt.Println("indexCollisionCounter", indexCollisionCounter, "iterations", i, "percent collisions", float64(indexCollisionCounter)/float64(i))
 			return
+		}
+		if tries[tableIndex].set {
+			indexCollisionCounter++
 		}
 		//fmt.Println("Replacing ", hex.EncodeToString(tries[tableIndex].digest[:]), " with ", hex.EncodeToString(digest))
 		copy(tries[tableIndex].digest[:], digest)
 		copy(tries[tableIndex].data[:], data)
-		/*
-			digestStr := string(digest)
-				if prev, ok := tries[digestStr]; ok {
-					// found collision
-					item1 = data
-					item2 = prev
-					digestOut = digest
-					return
-				}
-				tries[digestStr] = data
-		*/
+		tries[tableIndex].set = true
 	}
 
 	return nil, nil, nil
-}
-
-type sha512n struct {
-	n      int
-	sha512 hash.Hash
-}
-
-// SHA-512 truncated to first n bits, where 8 <= n <= 48 and n is a
-// multiple of 8.
-func NewSha512n(n int) (hash.Hash, error) {
-	if n%8 != 0 || n < 8 || n > 48 {
-		return nil, fmt.Errorf("n must be multiple of 8 between 8 and 48")
-	}
-	return &sha512n{n, sha512.New()}, nil
-}
-
-func (h *sha512n) BlockSize() int {
-	return h.sha512.Size()
-}
-
-// Sum appends the current hash to b and returns the resulting slice.
-// It does not change the underlying hash state.
-func (h *sha512n) Sum(b []byte) []byte {
-	sha512Sum := h.sha512.Sum(nil)
-	sha512NSum := sha512Sum[:h.Size()]
-	return append(b, sha512NSum...)
-}
-
-// Reset resets the Hash to its initial state.
-func (h *sha512n) Reset() {
-	h.sha512.Reset()
-}
-
-// Size returns the number of bytes Sum will return.
-func (h *sha512n) Size() int {
-	return h.n / 8
-}
-
-func (h *sha512n) Write(p []byte) (n int, err error) {
-	return h.sha512.Write(p)
 }
